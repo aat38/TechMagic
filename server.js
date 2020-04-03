e876a1661fb46b0a0902e1b65b955e2c979f650e
Glitch (hello-express) <none>
2020-04-03 19:00:46 +0000

    🍲👰 Checkpoint ./views/index.ejs:10221761/1817 ./views/search.ejs:10221761/662 ./server.js:10221761/170 ./README.md:10221761/3825

--- server.js
+++ server.js
@@ -5,18 +5,12 @@ const app = express();
 app.use('/public',express.static(path.join(__dirname)));
 app.set('view engine','ejs');
 
-// const viewsRoutes = require("/views");
-// const indexRoute = require("/index");
-
-// app.use("/", indexRoute);
-// app.use("/api/book", apiRouter);
-
-// app.get('/',(req,res)=>{
-//     res.render('index'
-//       )});
+app.get('/',(req,res)=>{
+    res.render('index'
+      )});
 
 app.get('/:userQuery',(req,res)=>{
-    res.render('index',{data : {userQuery: req.params.userQuery,
+    res.render('search',{data : {userQuery: req.params.userQuery,
                                searchResults : ['book1','book2','book3'],
                                loggedIn : true,
                                username : 'lkjslkjdf'}});
