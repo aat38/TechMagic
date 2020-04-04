819ec69d1be757a997c1b8c01bd44a098eff2d57
Glitch (hello-express) <none>
2020-04-04 03:29:53 +0000

    🚕🔋 Checkpoint ./views/index.ejs:10221761/238 ./server.js:10221761/1348 ./views/database.ejs:10221761/456 ./package.json:10221761/1320

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { Client } = require("pg");
+// import {Client} from "pg"; ^^^ is what line above is absically saying
+const client = new Client({
+  connectionString:process.env.DATABASE_URL,
+  //database url is stored in env 
+  ssl: true
+  //^ssl is true bc heroku reqires this 
+});
 
 app.use('/public',express.static(path.join(__dirname)));
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.render('index'
-      )});
+    res.render('index')
+});
 
 app.get('/:userQuery',(req,res)=>{
     res.render('search',{data : {userQuery: req.params.userQuery,
                                searchResults : ['book1','book2','book3'],
                                loggedIn : true,
                                username : 'lkjslkjdf'}});
+});
+
+app.get('/pages/database',(req,res)=>{
+    res.render('database', {data : })
 });
 
 
