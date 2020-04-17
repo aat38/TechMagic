e361992b0690c0cf937a388021bc1fed0e24554a
Glitch (ejs-views-practice) <none>
2020-04-17 19:39:16 +0000

    🎉⛽️ Checkpoint ./views/products.ejs:10221761/61 ./routes/api.js:10221761/67

--- routes/api.js
+++ routes/api.js
@@ -21,7 +21,7 @@ apirouter.get("/claims", (request, response) => {
     function(resp) {
       console.log("Successfully retrieved ALL claims and claims information");
       // console.log(resp.rows);
-      response.send(resp.rows);
+      response.json(resp);
       //this info can be further parsed on frontend ie: can view only issues from claims table, only employees or whatever combo of things. the important thing is that the data is here and ready to be manipulated
     },
     function(err) {
